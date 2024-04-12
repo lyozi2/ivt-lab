66c34ea85684ccb831d2caaf20f0256d92960b72
lyozi2 <urak_joozsi@yahoo.com>
2024-04-12 09:40:52 +0200

    kesz

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -87,6 +87,7 @@ public class GT4500 implements SpaceShip {
           firingSuccess = secondaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = false;
         }
+        
         break;
     }
 
